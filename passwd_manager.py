621431a144d051fd63db40785cee9fbab2ffe42f
Antonios Atlasis <34843366+aatlasis@users.noreply.github.com>
2023-05-28 10:55:00 +0200

    Add files via upload
    capability to update or delete an entry

--- passwd_manager.py
+++ passwd_manager.py
@@ -1,14 +1,16 @@
 import os, csv, datetime, getpass
+from tempfile import NamedTemporaryFile
+import shutil
 from cryptography.hazmat.primitives.kdf.scrypt import Scrypt
 from cryptography.hazmat.backends import default_backend
 
 special_characters = ['#','!','~','(',')','_']
+fieldnames = ['website', 'salt','timestamp']
 
-def generate_salt():
-    website = input('Enter the name of the website (e.g. facebook): ')
+def generate_salt(website):
     salt = os.urandom(16)
     current_time = datetime.datetime.now()
-    return website,salt,current_time
+    return salt,current_time
 
 def derive_hash(salt,length,special_character_needed):
     kdf = Scrypt(salt=salt, length=32, n=2**14, r=8, p=1, backend=default_backend())
@@ -30,15 +32,18 @@ def derive_hash(salt,length,special_character_needed):
         myhash = part1+special_characters[special_char]+part2
     print("KDF output:", myhash)
 
-def main():
-    fieldnames = ['website', 'salt','timestamp']
+def print_websites():
+    print('\n')
     with open('salts.csv', mode='r') as csv_file:
         csv_reader = csv.DictReader(csv_file,fieldnames=fieldnames)
         i=1
         for lines in csv_reader:
-            print(i,lines['website'])
+            print(i,lines['website'],lines['timestamp'])
             i=i+1 
-    choice=input('Enter "0" to read a salt, "1" to generate a new salt, any other integer to print available websites: ')
+
+def main():
+    print_websites()
+    choice=input('\nEnter:\n-------\n"0" to read a salt, \n"1" to generate a new salt, \n"2" to update an exisitng password, \n"3" to delete an existing entry, \nany other integer to print available websites: ')
     try:
         choice=int(choice)
     except:
@@ -66,7 +71,8 @@ def main():
             else:
                 print(f'salt for website with id {website_id} not found')
     elif choice ==1: 
-        website,salt,timestamp = generate_salt()
+        website = input('Enter the name of the website (e.g. facebook): ')
+        salt,timestamp = generate_salt(website)
         length = input('Enter the maximum allowed password length (enter 0 if there is no max length or if you do not know): ')
         try:
             length = int(length)
@@ -82,11 +88,49 @@ def main():
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
             writer.writerow({'website': ':'.join((website,str(length),str(special_character_needed))), 'salt': salt.hex(), 'timestamp': str(timestamp)})
         derive_hash(salt,length,special_character_needed)
-    else:
-        with open('salts.csv', mode='r') as csv_file:
+    elif choice ==2 or choice ==3: 
+        if choice ==2:
+            website_id = input('Enter the id number of the website you want to update the key: ')
+        else:
+            website_id = input('Enter the id number of the website you want to delete the key: ')
+        try:
+            website_id = int(website_id)
+        except:
+            print('you must enter an integer number here')
+            exit(0)
+        temp_file = NamedTemporaryFile(mode='w', delete=False)
+        filename = 'salts.csv'
+        with open(filename, mode='r') as csv_file, temp_file:
             csv_reader = csv.DictReader(csv_file,fieldnames=fieldnames)
+            csv_writer = csv.DictWriter(temp_file,fieldnames=fieldnames)
+            i = 1
             for lines in csv_reader:
-                print(lines['website'])
+                salt = lines['salt']
+                website = lines['website']
+                timestamp = lines['timestamp']
+                if i == website_id:
+                    website_name = lines['website'].split(':')
+                    if choice == 2:
+                        print('updating', website_name[0])
+                        salt,timestamp = generate_salt(website_name[0])
+                        csv_writer.writerow({'website': website, 'salt': salt.hex(), 'timestamp': str(timestamp)})
+                        derive_hash(salt,int(website_name[1]),int(website_name[2]))
+                    else:
+                        print('\nwebsite:',website_name[0])
+                        print('---------')
+                        confirmation = input('Are you sure to want to delete the key for this website (Y/y)? ')
+                        if confirmation == 'Y' or confirmation == 'y':
+                            print('deleting entry for',website_name[0])
+                        else:
+                            print('deletion of entry for',website_name[0],'cancelled')
+                            exit(0)
+                else:
+                    csv_writer.writerow({'website' : website, 'salt' : salt, 'timestamp' : timestamp})
+                i = i+1
+        shutil.move(temp_file.name, filename)
+        print_websites()
+    else:
+        print_websites()
 
 if __name__ == '__main__':
     main()
